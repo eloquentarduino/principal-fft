9d8fe68681d7ba523ed442b310eb34bafa587ab8
Agrimagsrl <web@agrimag.it>
2020-09-05 07:57:48 +0200

    working code

---
+++ principalfft/PrincipalFFT.py
@@ -0,0 +1,29 @@
+import numpy as np
+from numpy.fft import rfft
+
+
+class PrincipalFFT:
+    """Extract principal FFT components for features generation"""
+    def __init__(self, n_components):
+        self.n_components = n_components
+        self.original_size = 0
+        self.idx = None
+
+    def fit(self, X):
+        """Fit data (find the n top components of the FFT)"""
+        Xfft = rfft(X)
+        Xabs = np.abs(Xfft)
+        Xmax = Xabs.max(axis=0)
+        self.original_size = len(X[0])
+        self.idx = (-Xmax).argsort()[:self.n_components]
+        return self
+
+    def transform(self, X):
+        """Transform data"""
+        assert self.idx is not None, "PrincipalFFT instance not fitted"
+        return np.abs(rfft(X)[:, self.idx])
+
+    def fit_transform(self, X):
+        """Fit and transform"""
+        self.fit(X)
+        return self.transform(X)
---
+++ setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+setup(
+  name = 'principal-fft',
+  packages = ['principalfft'],
+  version = '1.0.1',
+  license='MIT',
+  description = 'Extract principal FFT components for features generation',
+  author = 'Simone Salerno',
+  author_email = 'eloquentarduino@gmail.com',
+  url = 'https://github.com/eloquentarduino/principal-fft',
+  download_url = 'https://github.com/eloquentarduino/principal-fft/archive/v_101.tar.gz',
+  keywords = [
+    'ML',
+    'sklearn',
+    'machine learning'
+  ],
+  install_requires=[
+    'numpy',
+  ],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
